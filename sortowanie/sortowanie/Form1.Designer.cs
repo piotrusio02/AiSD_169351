295ae5728b2b261b96f36a451eb1c36d5fd637e1
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2025-01-11 18:15:31 +0100

    Add files via upload

---
+++ sortowanie/sortowanie/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace sortowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Honeydew;
+            this.label1.Location = new System.Drawing.Point(32, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.label2.Location = new System.Drawing.Point(32, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(33, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sortowanie bąbelkowe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(354, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Sortowanie przez wstawianie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(37, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(353, 55);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sortowanie przez scalanie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(405, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(354, 55);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Sortowanie szybkie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(37, 332);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(722, 55);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Sortowanie przez zliczanie";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(545, 39);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(214, 80);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Generuj tablicę\r\n";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(545, 128);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(214, 20);
+            this.domainUpDown1.TabIndex = 9;
+            this.domainUpDown1.Text = "domainUpDown1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+    }
+}
+
---
+++ sortowanie/sortowanie/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortowanie
+{
+    public partial class Form1 : Form
+    {
+        int[] tab;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Przed: ";
+            label2.Text = "Po: ";
+            label3.Text = "Czas sortowania: ";
+            domainUpDown1.Text = "10";
+        }
+
+        private void button6_Click(object sender, EventArgs e) // generowanie tablicy
+        {
+            int rozmiar = int.Parse(domainUpDown1.Text);
+            tab = Generuj(rozmiar);
+            label1.Text = "Przed: " + string.Join(",", tab);
+            label2.Text = "Po: ";
+            label3.Text = "Czas sortowania: ";
+        }
+
+        public static int[] Generuj(int rozmiar)
+        {
+            Random random = new Random();
+            int[] tab = new int[rozmiar];
+            for(int i = 0;i< rozmiar; i++)
+            {
+                tab[i] = random.Next(1, 101);
+            }
+            return tab;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // sortowanie bąbelkowe
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int temp;
+            for(int i = 0; i< tab.Length; i++)
+            {
+                for(int j = 0; j < tab.Length - 1; j++)
+                {
+                    if (tab[j] > tab[j + 1])
+                    {
+                        temp = tab[j];
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = temp;
+                    }
+                }
+            }
+            watch.Stop();
+            label2.Text = "Po: " + string.Join(", ", tab);
+            label3.Text = "Czas sortowania: " + watch.Elapsed.TotalMilliseconds.ToString("F3") + " ms";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // sortowanie przez wstawianie
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for(int i = 1; i < tab.Length; i++)
+            {
+                int temp = tab[i];
+                int j = i - 1;
+
+                while(j >= 0 && tab[j] > temp)
+                {
+                    tab[j + 1] = tab[j];
+                    j--;
+                }
+                tab[j + 1] = temp;
+            }
+            watch.Stop();
+            label2.Text = "Po: " + string.Join(", ", tab);
+            label3.Text = "Czas sortowania: " + watch.Elapsed.TotalMilliseconds.ToString("F3") + " ms";
+        }
+
+        private void button3_Click(object sender, EventArgs e) // sortowanie przez scalanie
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            MergeSort(tab);
+
+            watch.Stop();
+            label2.Text = "Po: " + string.Join(", ", tab);
+            label3.Text = "Czas sortowania: " + watch.Elapsed.TotalMilliseconds.ToString("F3") + " ms";
+
+        }
+
+        public static void MergeSort(int[] tab)
+        {
+            if(tab.Length <= 1)
+            {
+                return;
+            }
+            int mid = tab.Length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[tab.Length - mid];
+
+            for(int i = 0; i < mid; i++)
+            {
+                left[i] = tab[i];
+            }
+            for(int i = mid;i< tab.Length; i++)
+            {
+                right[i - mid] = tab[i];
+            }
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(tab, left, right);
+        }
+
+        public static void Merge(int[] tab, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while(i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    tab[k++] = left[i++];
+                }
+                else
+                {
+                    tab[k++] = right[j++];
+                }
+            }
+            while( i < left.Length)
+            {
+                tab[k++] = left[i++];
+            }
+            while( j < right.Length)
+            {
+                tab[k++] = right[j++];
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // sortowanie szybkie
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            int left = 0;
+            int right = tab.Length - 1;
+            QuickSort(tab, left, right);
+
+            watch.Stop();
+            label2.Text = "Po: " + string.Join(", ", tab);
+            label3.Text = "Czas sortowania: " + watch.Elapsed.TotalMilliseconds.ToString("F3") + " ms";
+        }
+
+        public static void QuickSort(int[] tab, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Podzial(tab, left, right);
+                QuickSort(tab, left, pivot - 1);
+                QuickSort(tab, pivot + 1, right);
+            }
+        }
+
+        public static int Podzial(int[] tab, int left, int right)
+        {
+            int pivot = tab[right];
+            int i = left - 1;
+
+            for(int j = left; j < right; j++)
+            {
+                if (tab[j] <= pivot)
+                {
+                    i++;
+                    int temp = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = temp;
+                }
+            }
+            int temp1 = tab[i + 1];
+            tab[i + 1] = tab[right];
+            tab[right] = temp1;
+            return i + 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e) // sortowanie przez zliczanie
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            int max = tab.Max();
+            int[] count = new int[max + 1];
+
+            for(int i = 0; i < tab.Length; i++)
+            {
+                count[tab[i]]++;
+            }
+            int index = 0;
+            for(int i = 0; i <= max; i++)
+            {
+                while (count[i] > 0)
+                {
+                    tab[index++] = i;
+                    count[i]--;
+                }
+            }
+
+            watch.Stop();
+            label2.Text = "Po: " + string.Join(", ", tab);
+            label3.Text = "Czas sortowania: " + watch.Elapsed.TotalMilliseconds.ToString("F3") + " ms";
+        }
+    }
+}
